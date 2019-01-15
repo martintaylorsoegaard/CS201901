f3bf77e80765153938316d7bad2ec19cf812f6c8
Martin Taylor Søgaard <martintaylorsoegaard@gmail.com>
2019-01-15 12:26:59 +0100

    Opgave 3+4

---
+++ SimpleVariableOpgave3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleVariableOpgave3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = DateTime.Now;
+            d1 = d1.AddDays(10);
+            Console.WriteLine(d1);
+            d1 = d1.AddMinutes(20);
+            Console.WriteLine(d1);
+            d1 = d1.AddMonths(-1);
+            Console.WriteLine(d1);
+
+            DateTime d2 = new DateTime(2019, 01, 01);
+            DateTime d3 = new DateTime(2018, 06, 30);
+            Console.WriteLine((d2-d3).TotalDays);
+
+            TimeSpan t2 = new TimeSpan(16, 0, 0);
+            Console.WriteLine(t2);
+            TimeSpan t3 = new TimeSpan(0, 30, 0);
+            Console.WriteLine(t3);
+            TimeSpan t4 = t2 - t3;
+            Console.WriteLine(t4);
+            TimeSpan t5 = t2 + t3;
+            Console.WriteLine(t5);
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ SimpleVariableOpgave4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleVariableOpgave4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fornavn = "Mikkel";
+            string efternavn = "Cronberg";
+            string samletNavn = $"{fornavn} {efternavn}";
+            Console.WriteLine(samletNavn);
+            string navnStort = samletNavn.ToUpper();
+            Console.WriteLine(navnStort);
+            string navnLille = samletNavn.ToLower();
+            Console.WriteLine(navnLille);
+            string subNavn = navnLille.Substring(7, 4);
+            Console.WriteLine(subNavn);
+
+
+            string test = "Dette\ter\nen\n\rtest\rTE";
+            Console.WriteLine(test);
+            string tempPath = @"C:\temp\";
+            string newFolder = "TestFolder";
+            string newFolderPath = System.IO.Path.Combine(tempPath, newFolder);
+            System.IO.Directory.CreateDirectory(newFolderPath);
+            string totalPath = $"{newFolderPath}\\TestDoc.txt";
+            System.IO.File.WriteAllText(totalPath,test);
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
